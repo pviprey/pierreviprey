d501904350fb017208c01621e7326b7575ebd636
Miyne <mine.gaft@gmail.com>
2021-10-31 16:37:08 +0100

    ajout expériences et diplome

--- script/index.js
+++ script/index.js
@@ -1,5 +1,8 @@
 "use strict";
 
+const START_YEAR = 2014;
+const NOM_YEAR = new Date().getFullYear();
+
 class Coord{
     constructor(x, y){
         this.x = x;
@@ -7,32 +10,61 @@ class Coord{
     }
 }
 
+class Experience{
+    constructor(index, color, dateBegin, dateEnd = null){
+        this.index = index;
+        this.color = color;
+        this.dateBegin = dateBegin;
+        this.dateEnd = dateEnd;
+    }
+}
+
 document.addEventListener("DOMContentLoaded", function() {
     /** Récupération des informations liées au canvas */
     let canvas = document.getElementById("cvs");
     let limite = document.getElementById("limit");
-    console.log(limite);
-    console.log(canvas);
 
-    const WIDTH = canvas.width;
-    const HEIGHT = canvas.height;
+    const WIDTH = canvas.width = limite.offsetWidth;
+    const HEIGHT = canvas.height = limite.offsetHeight;
     let ctx = canvas.getContext("2d");
 
     let size_year = WIDTH/9;
 
-    /*
-    jQuery(document).ready(function() {
-        jQuery(canvas).draggable(); 
+    $(canvas).draggable({
+        axis: 'x',
+        drag: function(event, ui) {
+          var left = ui.position.left,
+              offsetWidth = ($(this).width() - $(this).parent().width()) * -1;
+      
+          if (left > 0) {
+            ui.position.left = 0;
+          }
+          if (offsetWidth > left) {
+            ui.position.left = offsetWidth;
+          }
+        }
     });
-    */
 
+    let tab_exp = [];
+
+    //Lycee: Sept 2014 -> Juin 2017
+    tab_exp.push(new Experience(0, "blue", new Date(2014, 8), new Date(2017, 5)));
+
+    //Universite: Sept 2017 -> Now
+    tab_exp.push(new Experience(0, "purple", new Date(2017, 8), new Date()));
+
+    //CSGO Team: Sept 2018 -> Avril 2019
+    tab_exp.push(new Experience(1, "red", new Date(2018, 8), new Date(2019, 3)));
+    
+    //LesJosettes: Avril 2021 -> Now
+    tab_exp.push(new Experience(1, "orange", new Date(2021, 3), new Date()));
+    
     for(let i = 0; i < 9; i++){
-        let year = 2014+i;
+        let year = START_YEAR + i;
         ctx.textAlign = "center";
         ctx.fillStyle = "black";
-        ctx.font = "10px helvetica";
-        console.log(size_year/2+size_year*i);
-        let position = new Coord(size_year/2+size_year*i, 10);
+        ctx.font = "15px helvetica";
+        let position = new Coord(size_year/2+size_year*i, 20);
         ctx.fillText(year, position.x, position.y);
 
         ctx.beginPath();
@@ -43,4 +75,36 @@ document.addEventListener("DOMContentLoaded", function() {
         ctx.stroke();
         ctx.closePath();
     }
+
+    for(let event of tab_exp){
+        let size = 30;
+
+        ctx.beginPath();
+        ctx.strokeStyle = event.color;
+        ctx.lineWidth = size;
+
+        let monthBegin = event.dateBegin.getMonth();
+        let yearBegin = event.dateBegin.getFullYear();
+        let positionBegin = new Coord(size_year/2 + (yearBegin - START_YEAR)*size_year + monthBegin/11*size_year, HEIGHT/2 + 40*event.index);
+        ctx.moveTo(positionBegin.x, positionBegin.y);
+
+        let monthEnd = event.dateEnd.getMonth();
+        let yearEnd = event.dateEnd.getFullYear();
+        let positionEnd = new Coord(size_year/2 + (yearEnd - START_YEAR)*size_year + monthEnd/11*size_year, HEIGHT/2 + 40*event.index);
+        ctx.lineTo(positionEnd.x, positionEnd.y);
+        
+        ctx.stroke();
+        ctx.closePath();
+
+        if(event.dateEnd == Date()){
+            console.log("oui");
+            ctx.beginPath();
+            ctx.fillStyle = event.color;
+            ctx.lineWidth = 1;
+            ctx.moveTo(positionEnd.x, positionEnd.y+size/2);
+            ctx.lineTo(positionEnd.x, positionEnd.y-size/2);
+            ctx.lineTo(positionEnd.x+size/2, positionEnd.y);
+            ctx.fill();
+        }
+    }    
 });
